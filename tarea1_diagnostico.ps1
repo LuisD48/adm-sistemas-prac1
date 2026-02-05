9ab2299193931d129d94b42b07607be7604dd84c
LuisD48 <luislarsotropo321@gmail.com>
2026-02-05 14:03:32 -0700

    Cambio de color y un salto de linea en PowerShell v1.1

--- tarea1_diagnostico.ps1
+++ tarea1_diagnostico.ps1
@@ -2,6 +2,7 @@ Set-Content -Path "C:\tarea_diagnostico.ps1" -Value @"
 Write-Host "=============================" -ForegroundColor Blue
 Write-Host "  DIAGNOSTICO - SRV-WINDOWS  " -ForegroundColor Blue
 Write-Host "=============================" -ForegroundColor Blue
+Write-Host ""
 Write-Host "HOSTNAME: $(hostname)"
 Write-Host "IP INTERNA: $((Get-NetIPAddres -InterfaceAlias '*Ethernet 2*' -AddressFamily IPv4).IPAddress)"
 Write-Host "Disco: $((Get-Volume C).SizeRemaining / 1GB) GB Libres"
